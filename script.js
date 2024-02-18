3cf415a09b1c708cb7f2459cb5af9b581aa0640a
Ilenia M <nethleen@gmail.com>
2024-02-18 22:53:36 +0100

    feat: add calculation of number of combinations based on number of stamps and exponent value
    The changes in this commit add a calculation for the number of combinations based on the number of different stamp denominations and the exponent value. This calculation is displayed in the UI and is updated dynamically when the number of stamps or the exponent value changes. This provides users with more information about the potential number of combinations they can create with their stamps.

--- script.js
+++ script.js
@@ -68,9 +68,14 @@ function showStamps() {
     fieldset.innerHTML = html
     aside.innerHTML = `<p>Numero totale francobolli: ${totalAmount}</p>
     <p>Valore totale: €${(totalValue/100).toFixed(2)}</p>
-    <p>Numero denominazioni diverse: ${myStamps.length}</p>`
+    <p>Numero denominazioni diverse: ${myStamps.length}</p>
+    <p>Numero combinazioni calcolate: <span id="numb-comb">${Math.pow(myStamps.length, numeroFrancobolli.value).toExponential()}</span></p>`
 
 }
+
+numeroFrancobolli.addEventListener("change", () => {
+    document.querySelector('#numb-comb').innerText = Math.pow(myStamps.length, numeroFrancobolli.value).toExponential()
+})
 
 function removeStamp(fv) {
     myStamps.find(x => x.face_value === fv).number = 0
